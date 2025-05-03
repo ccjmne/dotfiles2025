4a2b4061626609c365b61c22c757fba1174498f5
Éric NICOLAS <ccjmne@gmail.com>
2025-05-03 14:28:19 +0200

    Set $PATH before attempting setting $EDITOR
    In case Neovim is built from Git and installed to my XDG_DATA_HOME, but
    preferrable in principle in any case.

--- home/config/zsh/env.zsh
+++ home/config/zsh/env.zsh
@@ -1,12 +1,6 @@
 export ZLE_SPACE_SUFFIX_CHARS=$'&|'         # Avoid 'consuming' the space before a pipe or ampersand
 export PARINIT="rTbgqR B=.,?'_A_a_@ Q=_s>|" # TODO: Figure out how to make this available to nvim shell
 
-if [[ $(command -v nvim) ]]; then
-  export EDITOR="nvim"
-  export VISUAL="nvim"
-  export MANPAGER='nvim +Man!'
-fi
-
 export MISE_CONFIG_FILE="$XDG_CONFIG_HOME/mise.toml"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 
@@ -16,3 +10,9 @@ path=(
     "$CARGO_HOME/bin"
     "$XDG_DATA_HOME/nvim/mason/bin"
 )
+
+if [[ $(command -v nvim) ]]; then
+  export EDITOR="nvim"
+  export VISUAL="nvim"
+  export MANPAGER='nvim +Man!'
+fi
