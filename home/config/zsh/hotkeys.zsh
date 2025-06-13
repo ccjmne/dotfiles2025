bf814b5fee3fb77b66e33c6e8ddc985404b8ffca
Éric NICOLAS <ccjmne@gmail.com>
2025-06-14 01:41:19 +0200

    Introduce Zsh C-A hotkey to jump to nearest anchor
    As defined by Powerlevel10k

--- home/config/zsh/hotkeys.zsh
+++ home/config/zsh/hotkeys.zsh
@@ -8,3 +8,24 @@ function resume {
 }
 zle -N resume
 bindkey '^Z' resume
+
+# Ctrl-A to jump to the nearest anchor directory
+local anchor_files=(.bzr .citc .git .hg .node-version .python-version .go-version .ruby-version .lua-version .java-version .perl-version .php-version .tool-versions .mise.toml .shorten_folder_marker .svn .terraform CVS Cargo.toml composer.json go.mod package.json stack.yaml)
+function anchor {
+  local dir="$(dirname "$PWD")"
+  while [[ "$dir" != "/" ]]; do
+    for anchor in "${anchor_files[@]}"; do
+      if [[ -e "$dir/$anchor" ]]; then
+        echo "$dir"
+        cd ${(q)dir}
+        zle push-input
+        BUFFER=''
+        zle accept-line
+        return 0
+      fi
+    done
+    dir="$(dirname "$dir")"
+  done
+}
+zle -N anchor
+bindkey '^A' anchor
