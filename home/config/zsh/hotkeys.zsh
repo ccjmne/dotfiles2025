58d259ddc5c46f25471afee337e2be4f458a3309
Éric NICOLAS <ccjmne@gmail.com>
2025-06-14 01:42:01 +0200

    Create update command for Zsh C-A hotkey

--- home/config/zsh/hotkeys.zsh
+++ home/config/zsh/hotkeys.zsh
@@ -10,6 +10,7 @@ zle -N resume
 bindkey '^Z' resume
 
 # Ctrl-A to jump to the nearest anchor directory
+# exe "r!sed -n '/anchor_files=/,/)$/p' <(curl -sL https://github.com/romkatv/powerlevel10k/raw/refs/heads/master/config/p10k-classic.zsh)" | normal! V%J==F(lx
 local anchor_files=(.bzr .citc .git .hg .node-version .python-version .go-version .ruby-version .lua-version .java-version .perl-version .php-version .tool-versions .mise.toml .shorten_folder_marker .svn .terraform CVS Cargo.toml composer.json go.mod package.json stack.yaml)
 function anchor {
   local dir="$(dirname "$PWD")"
