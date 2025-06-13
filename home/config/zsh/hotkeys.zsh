f2bece0d98a8ff666aded7b8cd451478e96e3ff9
Éric NICOLAS <ccjmne@gmail.com>
2025-06-14 01:40:29 +0200

    Lightly polish comment to C-Z Zsh hotkey

--- home/config/zsh/hotkeys.zsh
+++ home/config/zsh/hotkeys.zsh
@@ -1,5 +1,5 @@
+# Ctrl-Z to resume background job without polluting the history
 # Source: https://www.reddit.com/r/vim/comments/9bm3x0/ctrlz_binding/
-# Allow Ctrl-Z to toggle between suspend and resume
 function resume {
     fg
     zle push-input
