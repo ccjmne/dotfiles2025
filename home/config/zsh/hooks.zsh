a74739ab90839eca008a6aa097ee43bcdd00cfd1
Éric NICOLAS <ccjmne@gmail.com>
2025-06-14 04:11:48 +0200

    Fix chpwd hook (ls isn't a Zsh function anymore)

--- home/config/zsh/hooks.zsh
+++ home/config/zsh/hooks.zsh
@@ -1,5 +1,6 @@
 update_gpg_tty() gpg-connect-agent updatestartuptty /bye &>/dev/null
+lsfn() ls
 
 autoload -U add-zsh-hook
 add-zsh-hook preexec update_gpg_tty
-add-zsh-hook chpwd   ls
+add-zsh-hook chpwd   lsfn
