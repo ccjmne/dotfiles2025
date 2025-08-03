842e0e28be8999b656c4a4293e040ee469493c10
Éric NICOLAS <ccjmne@gmail.com>
2025-08-03 18:10:36 +0200

    Leverage --entry-type filter on klog tags
    Klog is fantastic.

--- home/config/zsh/p10k-more.zsh
+++ home/config/zsh/p10k-more.zsh
@@ -1,18 +1,9 @@
-# set to list of bookmarks to read from, leave empty to parse all
-bks=()
+function klog-wip() {
+    for bk in $(klog bookmarks list | grep -Eo '^[^ ]+'); do
+        klog tags $bk --today -vun --entry-type OPEN_RANGE --no-style --no-warn | sed -E "/\b0m/d; s/^\(untagged\)/$bk/; s/ +/ /g"
+    done
+}
 
 function prompt_klog() {
-    tasks=()
-    now=$(date +%H:%M | sed 's/:/*60+/' | bc)
-    for bk in ${bks:-$(klog bookmarks list | grep -Eo '^[^ ]+')}; do
-        { read start; read tags; } <<< $(klog json "$bk" --today \
-            | jq '.records[].entries[] | select(.type == "open_range") | .start_mins, (.tags? | join(" "))' -r)
-        if [ -n "$start" ]; then
-            d=$(($now - $start))
-            dh="$((d / 60))h$((d % 60))m"
-            tasks+=("${tags:-$bk} ${dh#0h}")
-        fi
-    done
-    out=$(printf ", %s" "${tasks[@]}")
-    p10k segment -f4 -t"${out:2}"
+    p10k segment -f4 -t"$(klog-wip | paste -sd',' | sed 's/ *,/, /g')"
 }
