bb5199a8c5a5aae6e3f3500771f8139f20abf3c2
Éric NICOLAS <ccjmne@gmail.com>
2025-08-07 23:25:40 +0200

    Adopt clipboard-indicator GNOME extension

--- scripts/gnome-ext.sh
+++ scripts/gnome-ext.sh
@@ -20,7 +20,9 @@ while IFS=: read id name; do
         gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Extensions.EnableExtension "$id" > /dev/null
     fi
 done <<EOF
-just-perfection-desktop@just-perfection:just-perfection
-awesome-tiles@velitasali.com:awesome-tiles
-Vitals@CoreCoding.com:vitals
+awesome-tiles@velitasali.com
+clipboard-indicator@tudmotu.com
+just-perfection-desktop@just-perfection
+Vitals@CoreCoding.com
+weeks-start-on-monday@extensions.gnome-shell.fifi.org
 EOF
