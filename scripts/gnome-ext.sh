f2e67ff43aff794f2ef4fcdfd71aabaa5f00f976
Éric NICOLAS <ccjmne@gmail.com>
2025-08-07 22:58:06 +0200

    Drop disfavoured GNOME shell extensions
    - sound-output-device-chooser
        is essentially rendered obsolete by GNOME 48
    - burn-my-windows
        just doesn't really fit my current style

--- scripts/gnome-ext.sh
+++ scripts/gnome-ext.sh
@@ -5,24 +5,22 @@
 # TODO: Bring GNOME extensions configs. to this repository,
 #       Routinely dump them here with systemd
 
-while IFS=: read extension extension_dconf; do
-    if [ -e "$XDG_CONFIG_HOME/gnome-ext/$extension_dconf.ini" ]; then
-        dconf load "/org/gnome/shell/extensions/$extension_dconf/" < "$XDG_CONFIG_HOME/gnome-ext/$extension_dconf.ini"
+while IFS=: read id name; do
+    if [ -e "$XDG_CONFIG_HOME/gnome-ext/$name.ini" ]; then
+        dconf load "/org/gnome/shell/extensions/$name/" < "$XDG_CONFIG_HOME/gnome-ext/$name.ini"
     fi
 
     status=$(gdbus call                \
         --session                      \
         --object-path /org/gnome/Shell \
         --dest        org.gnome.Shell  \
-        --method      org.gnome.Shell.Extensions.InstallRemoteExtension "$extension")
+        --method      org.gnome.Shell.Extensions.InstallRemoteExtension "$id")
 
     if ! echo "$status" | grep -q "cancelled"; then
-        gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Extensions.EnableExtension "$extension" > /dev/null
+        gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Extensions.EnableExtension "$id" > /dev/null
     fi
 done <<EOF
 just-perfection-desktop@just-perfection:just-perfection
-sound-output-device-chooser@kgshank.net:
-burn-my-windows@schneegans.github.com:burn-my-windows
 awesome-tiles@velitasali.com:awesome-tiles
 Vitals@CoreCoding.com:vitals
 EOF
