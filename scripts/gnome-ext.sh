6090f5dc64c186a565828c78a7ca4b9266f70c10
Éric NICOLAS <ccjmne@gmail.com>
2025-08-07 22:16:07 +0200

    Make gnome-ext.sh POSIX-compliant

--- scripts/gnome-ext.sh
+++ scripts/gnome-ext.sh
@@ -1,38 +1,28 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
 # TODO: Install "weeks start on monday again"
 # TODO: Install "clipboard indicator"
 # TODO: Bring GNOME extensions configs. to this repository,
 #       Routinely dump them here with systemd
 
-declare -A extensions=(
-    ["just-perfection-desktop@just-perfection"]="just-perfection"
-    ["sound-output-device-chooser@kgshank.net"]=""
-    ["burn-my-windows@schneegans.github.com"]="burn-my-windows"
-    ["awesome-tiles@velitasali.com"]="awesome-tiles"
-    ["Vitals@CoreCoding.com"]="vitals"
-)
-
-for extension in "${!extensions[@]}"; do
-    extension_dconf="${extensions[$extension]}"
-
-    gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Extensions.DisableExtension "$extension" > /dev/null
-
-    if [[ -e "home/.config/$extension_dconf.ini" ]]; then
-        dconf load "/org/gnome/shell/extensions/$extension_dconf/" < "home/.config/$extension_dconf.ini"
+while IFS=: read extension extension_dconf; do
+    if [ -e "$XDG_CONFIG_HOME/gnome-ext/$extension_dconf.ini" ]; then
+        dconf load "/org/gnome/shell/extensions/$extension_dconf/" < "$XDG_CONFIG_HOME/gnome-ext/$extension_dconf.ini"
     fi
 
-    status=$(gdbus call \
-        --session \
-        --dest org.gnome.Shell \
+    status=$(gdbus call                \
+        --session                      \
         --object-path /org/gnome/Shell \
-        --method org.gnome.Shell.Extensions.InstallRemoteExtension "$extension")
+        --dest        org.gnome.Shell  \
+        --method      org.gnome.Shell.Extensions.InstallRemoteExtension "$extension")
 
-    if [[ "$extension" = "Vitals@CoreCoding.com" ]] && [[ $status =~ "successful" ]]; then
-        sudo apt-get install gir1.2-gtop-2.0 lm-sensors -y # See https://github.com/corecoding/Vitals/wiki/Storage-reporting-%22No-Data%22
-    fi
-
-    if [[ ! $status =~ "cancelled" ]]; then
+    if ! echo "$status" | grep -q "cancelled"; then
         gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Extensions.EnableExtension "$extension" > /dev/null
     fi
-done
+done <<EOF
+just-perfection-desktop@just-perfection:just-perfection
+sound-output-device-chooser@kgshank.net:
+burn-my-windows@schneegans.github.com:burn-my-windows
+awesome-tiles@velitasali.com:awesome-tiles
+Vitals@CoreCoding.com:vitals
+EOF
