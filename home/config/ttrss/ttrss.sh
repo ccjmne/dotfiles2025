9f3b77e5727858bfd1b9839ef77a07e01f670837
Éric NICOLAS <ccjmne@gmail.com>
2025-09-16 01:40:27 +0200

    Add --clean --if-exists to pg_dump flags

--- home/config/ttrss/ttrss.sh
+++ home/config/ttrss/ttrss.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 case "$1" in
-    dump)    docker exec -i ttrss_db pg_dump --no-acl --no-owner --user ttrss --dbname ttrss > ttrss.sql ;;
-    restore) docker exec -i ttrss_db psql                        --user ttrss --dbname ttrss < ttrss.sql ;;
+    dump)    docker exec -i ttrss_db pg_dump --user ttrss --dbname ttrss --clean --if-exists --no-acl --no-owner > ttrss.sql ;;
+    restore) docker exec -i ttrss_db psql    --user ttrss --dbname ttrss                                         < ttrss.sql ;;
     *)       echo "Usage: $0 <dump|restore>" 1>&2 && exit 1 ;;
 esac
