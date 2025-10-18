78393a378f0b105b83b521d9570ab10dd6edc800
Éric NICOLAS <ccjmne@gmail.com>
2025-10-18 21:33:33 +0200

    Work around hierarchical app_id in Mod+T
    Take the example of Ghostty:
    
        - its *app_id* is            "com.mitchellh.ghostty"
        - its *executable* is simply "ghostty"

--- home/bin/niri-run-or-raise-rotate.sh
+++ home/bin/niri-run-or-raise-rotate.sh
@@ -1,57 +1,32 @@
 #! /bin/sh
 
-# From /u/hypnomarten on Reddit:
+# Adapted from /u/hypnomarten's script at:
 # https://www.reddit.com/r/niri/comments/1mwnoil/niri_run_or_raise_focus_app_rotate_between/
 
-APP=$1
+APP_ID=$1
+INSTANCES=( $(niri msg --json windows | jq ".[] | select(.app_id == \"${APP_ID}\")" | jq '.["id"]') )
 
-# there may be instances of the app
-declare -a INSTANCES
-
-# get id of every possible instance
-INSTANCES=($(niri msg -j windows | jq ".[] | select(.app_id==\"$(echo $APP)\")" | jq '.["id"]'))
-
-# if there is no emacs: launch emacs
+# No instances found, spawn a new one
 if [ ${#INSTANCES[@]} -eq 0 ]; then
-    niri msg action spawn -- $APP
-else
-    # if there is only one instance, focus it
-    if [ ${#INSTANCES[@]} -eq 1 ]; then
-        # echo "Found only one instance - focussing now."
-        niri msg action focus-window --id $INSTANCES
-    else
-        # if there are more instances
-        # sort them
-        ISORTED=( $( printf "%s\n" "${INSTANCES[@]}" | sort -n ) )
-
-        # get id of currently focused window
-        FOCUSED=$(niri msg -j focused-window | jq '.id')
-
-        # if no instance is in focus, focus the first one
-        if [[ ! " ${ISORTED[@]} " =~ ${FOCUSED} ]]; then
-
-            # app not in focus, so focus first instance
-            niri msg action focus-window --id ${ISORTED[0]}
-        else
-            # if an instance is already in focus, focus next one
-            count=-1
-            for i in "${ISORTED[@]}"
-            do
-                ((count=$count+1))
-
-                if [ $i -eq ${FOCUSED} ]; then
-                    # if instance is in focus, get next index
-                    ((new_index=$count+1))
-                fi
-            done
-
-            # if focus is already on the last instance, focus on first instance
-            if [[ "$new_index" -eq "${#INSTANCES[@]}" ]]; then
-                ((new_index=0))
-            fi
-
-            # if an instance is in focus, focus next one
-            niri msg action focus-window --id ${ISORTED[new_index]}
-        fi
-    fi
+    # strip ^.*\.
+    # For example, we spawn "ghostty", whose app_id is "com.mitchellh.ghostty"
+    niri msg action spawn -- ${APP_ID##*.} && exit
 fi
+
+# Only one instance found, focus it
+if [ ${#INSTANCES[@]} -eq 1 ]; then
+    niri msg action focus-window --id $INSTANCES && exit
+fi
+
+# Multiple instances found, rotate through them
+ISORTED=( $( printf "%s\n" ${INSTANCES[@]} | sort -n ) )
+FOCUSED=$(niri msg --json focused-window | jq '.id')
+if [[ ! ${ISORTED[*]} =~ ${FOCUSED} ]]; then
+    niri msg action focus-window --id ${ISORTED[0]} && exit
+fi
+
+for i in ${!ISORTED[@]}; do
+    if [[ ${ISORTED[$i]} -eq $FOCUSED ]]; then
+        niri msg action focus-window --id ${ISORTED[$(((i + 1) % ${#INSTANCES[@]}))]} && exit
+    fi
+done
