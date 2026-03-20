ecf41c0e9342e875dd61c83dcc88b8f61b817cb2
Éric NICOLAS <ccjmne@gmail.com>
2026-03-20 01:47:18 +0100

    Push a couple of additional preferences for zen

--- home/share/zen/ccjmne/user.js
+++ home/share/zen/ccjmne/user.js
@@ -1,3 +1,6 @@
 user_pref("services.sync.username", "ccjmne@gmail.com");
+user_pref("sidebar.visibility", "hide-sidebar");
 user_pref("zen.theme.border-radius", 0);
 user_pref("zen.theme.content-element-separation", 0);
+user_pref("zen.workspaces.continue-where-left-off", true);
+user_pref("zen.workspaces.force-container-workspace", true);
