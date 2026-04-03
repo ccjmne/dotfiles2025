fd074a08f8b85929fadac25a7aba528c39bf4f65
Éric NICOLAS <ccjmne@gmail.com>
2026-04-03 03:45:49 +0200

    Completely overhaul Zsh key bindings configuration

--- home/config/zsh/hotkeys.zsh
+++ home/config/zsh/hotkeys.zsh
@@ -1,46 +1,49 @@
-# Ctrl-Z to resume background job without polluting the history
-# Source: https://www.reddit.com/r/vim/comments/9bm3x0/ctrlz_binding/
-function resume {
-    fg
-    zle push-input
-    BUFFER=''
-    zle accept-line
-}
-zle -N resume
-bindkey '^Z' resume
+# TODO: rename to "bindings" or something in that vein
 
-# Ctrl-A to jump to the nearest anchor directory
+export KEYTIMEOUT=1 # 1/100th of a second, essentially disabling keymaps timeout
+
+# Insert mode bindings
+# --------------------
+bindkey   ^P history-search-backward
+bindkey   ^N history-search-forward
+bindkey   ^A beginning-of-line                             # some love for emacs
+bindkey   ^E end-of-line                                   # some love for emacs
+bindkey   ^R history-incremental-search-backward
+bindkey   ^F history-incremental-search-forward
+bindkey   ^Z zle-resume                        # Ctrl-Z to resume background job
+bindkey \\ea zle-anchor                   # Meta-A to jump to the nearest anchor
+bindkey   ^I zle-expand-any                           # Tab also expands aliases
+bindkey   ^_ undo                  # Ctrl-/ undoes changes, such as *expansions*
+
+# Vi mode Configuration
+# ---------------------
+zstyle :zle:edit-command-line editor $EDITOR +'set ft=sh'
+bindkey -v
+bindkey -M vicmd  v edit-command-line   # disable visual mode, invoke fc instead
+bindkey -M vicmd ^V edit-command-line
+bindkey          ^V edit-command-line
+bindkey -M vicmd \? history-incremental-pattern-search-backward
+bindkey -M vicmd  / history-incremental-pattern-search-forward
+
+# Custom functions for ZLE
+# ------------------------
+function zle-expand-any() { zle _expand_alias || zle expand-or-complete }
+zle -N zle-expand-any
+
+function zle-resume { fg; zle push-input; BUFFER=''; zle accept-line }
+zle -N zle-resume
+
 # exe "r!sed -n '/anchor_files=/,/)$/p' <(curl -sL https://github.com/romkatv/powerlevel10k/raw/refs/heads/master/config/p10k-classic.zsh)" | normal! V%J==F(lx
 local anchor_files=(.bzr .citc .git .hg .node-version .python-version .go-version .ruby-version .lua-version .java-version .perl-version .php-version .tool-versions .mise.toml .shorten_folder_marker .svn .terraform CVS Cargo.toml composer.json go.mod package.json stack.yaml)
-function anchor {
-  local dir="$(dirname "$PWD")"
+function zle-anchor {
+  local dir=${PWD:h}
   while [[ "$dir" != "/" ]]; do
     for anchor in "${anchor_files[@]}"; do
       if [[ -e "$dir/$anchor" ]]; then
-        cd ${(q)dir}
-        zle push-input
-        BUFFER=''
-        zle accept-line
-        return 0
+        cd ${(q)dir}; zle push-input; BUFFER=''; zle accept-line; return 0
       fi
     done
-    dir="$(dirname "$dir")"
+    dir=${dir:h}
   done
 }
-zle -N anchor
-bindkey '^A' anchor
-
-# Vi Mode Configuration --------------------------------------------------------
-bindkey -v
-bindkey -M vicmd  'v' edit-command-line  # disable visual mode, deferring to edit-command-line instead
-bindkey -M vicmd '^V' edit-command-line
-bindkey -M viins '^V' edit-command-line
-export KEYTIMEOUT=1 # 1/100th of a second, faster transition between Vi modes
-bindkey -M vicmd  '?' history-incremental-pattern-search-backward
-bindkey -M vicmd  '/' history-incremental-pattern-search-forward
-bindkey          '^R' history-incremental-search-backward
-bindkey          '^F' history-incremental-search-forward
-bindkey          '^P' history-search-backward
-bindkey          '^N' history-search-forward
-bindkey          '^A' beginning-of-line
-bindkey          '^E' end-of-line
+zle -N zle-anchor
