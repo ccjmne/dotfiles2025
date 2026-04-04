5dfce97d33efec92b31ddbfde0823be3c4a0db34
Éric NICOLAS <ccjmne@gmail.com>
2026-04-05 01:54:48 +0200

    Tweak Zsh history options to (personal) perfection

--- home/config/zsh/opts.zsh
+++ home/config/zsh/opts.zsh
@@ -1,27 +1,26 @@
-setopt interactive_comments   # Allow comments in interactive shell
-setopt nocaseglob             # Case insensitive globbing (used in pathname expansion)
+setopt INTERACTIVE_COMMENTS     # Allow comments in interactive shell
+setopt NOCASEGLOB               # Case insensitive globbing (used in pathname expansion)
 
-# History-related opts =========================================================
-
+# History-related opts
+# --------------------
+SAVEHIST=8192                   # Maximum number of entries in $HISTFILE size
+HISTSIZE=16384                  # Maximum number of entries in memory
 HISTFILE="$ZDOTDIR/history"
-HISTSIZE=10000
-SAVEHIST=20000
 
-setopt hist_ignore_dups       # Ignore duplicate commands history list
-setopt hist_ignore_all_dups   # Delete old recorded dupes
-setopt hist_find_no_dups      # Do not display a line previously found
-setopt hist_save_no_dups      # Don't write duplicate entries in the history file
+setopt APPEND_HISTORY           # Append history to the history file (no overwriting)
+setopt EXTENDED_HISTORY         # Remove superfluous blanks from history entries
+setopt HIST_FIND_NO_DUPS        # Do not display a line previously found
+setopt HIST_IGNORE_ALL_DUPS     # Delete old recorded dupes
+setopt HIST_IGNORE_DUPS         # Ignore duplicate commands history list
+setopt HIST_REDUCE_BLANKS       # Remove superfluous blanks from history entries
+setopt HIST_SAVE_NO_DUPS        # Don't write duplicate entries in the history file
+setopt INC_APPEND_HISTORY_TIME  # Append to $HISTFILE upon command completion, not only on session close
 
-setopt hist_reduce_blanks     # Remove superfluous blanks from history entries
-setopt appendhistory          # Append history to the history file (no overwriting)
-setopt inc_append_history     # Immediately append to the history file, not just when a term is killed
-setopt share_history          # Share history between terminals
-
-# cd-related opts ==============================================================
-
-setopt auto_cd                # Attempt to cd on failing command corresponding to an existing directory
-setopt auto_pushd             # Push the current directory visited on the stack
-setopt cd_silent              # Do not print the directory stack after pushd or popd
-setopt pushd_silent           # Do not print the directory stack after pushd or popd
-setopt pushd_ignore_dups      # Do not store duplicates in the stack
-setopt pushdminus             # Switch '+' and '-' when used with a number to specify a stack entry
+# cd-related opts
+# ---------------
+setopt AUTO_CD                  # Attempt to cd on failing command corresponding to an existing directory
+setopt AUTO_PUSHD               # Push the current directory visited on the stack
+setopt CD_SILENT                # Do not print the directory stack after pushd or popd
+setopt PUSHD_SILENT             # Do not print the directory stack after pushd or popd
+setopt PUSHD_IGNORE_DUPS        # Do not store duplicates in the stack
+setopt PUSHDMINUS               # Switch '+' and '-' when used with a number to specify a stack entry
