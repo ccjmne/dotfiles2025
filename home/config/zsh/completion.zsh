5fc6040724efa4d46bc1f175edc050c99c356083
Éric NICOLAS <ccjmne@gmail.com>
2026-04-06 15:58:50 +0200

    Celebrate omni-tab achievement with rename, polish

--- home/config/zsh/completion.zsh
+++ home/config/zsh/completion.zsh
@@ -1,11 +1,20 @@
 zmodload zsh/complist
 autoload -U compinit && compinit
+autoload -U _generic
+zle -C zle-expand-omni expand-or-complete _generic
 
+zstyle ':completion:zle-expand-omni:*' completer _expand_alias _complete _ignored
+zstyle ':completion:*' menu select                                # enter menu mode when Tab again after options were displayed
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z-_}={A-Za-z_-}' `# Make the completion case- and [-_]- insensitive` \
+                                       '+l:|=* r:|=*'             # match sub-strings
+                                                                  # See https://zsh.sourceforge.io/Doc/Release/Completion-Widgets.html#Completion-Matching-Control
+                                                                  # See https://stackoverflow.com/questions/7906078/how-does-the-matcher-list-arguments-work-in-zsh-zstyle-completion
+
+bindkey               '^I'   zle-expand-omni          # Tab also expands aliases
 bindkey -M menuselect '^?'   backward-delete-char     # let Backspace behave in isearch
 bindkey -M menuselect '^H'   backward-delete-char     # let ^H        behave in isearch
 bindkey -M menuselect  '/'   history-incremental-search-forward
 bindkey -M menuselect  '?'   history-incremental-search-backward
-bindkey               '^I'   zle-expand-any           # Tab also expands aliases
 
 bindkey -M menuselect '^[h'  vi-backward-char         # navigate options grid with Meta-[hjkl]
 bindkey -M menuselect '^[k'  vi-up-line-or-history    #
@@ -15,13 +24,3 @@ bindkey -M menuselect '^N'   vi-down-line-or-history  # navigate otpions grid wi
 bindkey -M menuselect '^P'   vi-up-line-or-history    #
 bindkey -M menuselect '^I'   vi-down-line-or-history  # navigate otpions grid with Tab, S-Tab
 bindkey -M menuselect '^[[Z' vi-up-line-or-history    #
-
-autoload -U _generic
-zle -C zle-expand-any expand-or-complete _generic
-
-zstyle ':completion:zle-expand-any:*' completer _expand_alias _complete _ignored
-zstyle ':completion:*' menu select                                # enter menu mode when Tab again after options were displayed
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z-_}={A-Za-z_-}' `# Make the completion case- and [-_]- insensitive` \
-                                       '+l:|=* r:|=*'             # match sub-strings
-                                                                  # See https://zsh.sourceforge.io/Doc/Release/Completion-Widgets.html#Completion-Matching-Control
-                                                                  # See https://stackoverflow.com/questions/7906078/how-does-the-matcher-list-arguments-work-in-zsh-zstyle-completion
