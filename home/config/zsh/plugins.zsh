a5c78a07fc1f8cd1ca90cb33982874eb47b049d0
Éric NICOLAS <ccjmne@gmail.com>
2026-05-12 01:44:48 +0200

    Absorb all fzf environment variables in dot-zshenv

--- home/config/zsh/plugins.zsh
+++ home/config/zsh/plugins.zsh
@@ -7,8 +7,6 @@
 # source "$ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 # # source "$ZSH_PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-export FZF_DEFAULT_COMMAND='rg --files --hidden'    # Search hidden files w/ fzf
-export FZF_CTRL_T_COMMAND='rg --files 2> /dev/null' # Ctrl-T searches only visible files
 source /usr/share/fzf/key-bindings.zsh              # ~    0.5 ms
 # source /usr/share/fzf/completion.zsh              # ~    1   ms
 source <(zsh-patina activate)                       # ~    2   ms
